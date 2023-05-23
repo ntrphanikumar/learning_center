7c83712703f70a5fbc56183e5a2669c866113e52
Sai Bhavya Indla <saibhavya@yupptv.com>
2023-05-23 17:22:54 +0530

    updated_code

--- usermanagement/src/Components/Formm.js
+++ usermanagement/src/Components/Formm.js
@@ -3,42 +3,44 @@ import React, { useState } from 'react'
 import './MyStyleSheet.css';
 import { useNavigate } from 'react-router-dom';
 
-function Formm(EditData) {
+function Formm({editData}) {
   let localData = localStorage.getItem('data')
   localData = JSON.parse(localData)
   const navigate = useNavigate();
-    const [data, setData] = useState(localData ? localData : []);
-    const [ user, setUser ] = useState( EditData ? EditData?.editData : {
-      username:"", email:"", phone:"", password:"", gender:"", experience:"", ctc:"", location:""
-    });
-    const handleChange = (e) => {
-        const name = e.target.name;
-        const value = e.target.value;
-        setUser({...user, [ name ] : value});
-    }
-
-    const submitForm = (e) => {
-        e.preventDefault();
-        if (EditData?.editData) {
-         const updatedData = data.map((item) =>
-      item.phone === EditData.editData.phone ? user : item
-    );
-    setData(updatedData);
-      localStorage.setItem('data',JSON.stringify(updatedData));
-        }else{
-          setData([...data, user]);
-          localStorage.setItem('data',JSON.stringify([...data,user]));
-         navigate('/list')
-        }
-      
-    }
-
+  const [data, setData] = useState(localData ? localData : []);
+  const [user, setUser] = useState(editData ? editData : {
+    username: "", email: "", phone: "", password: "", gender: "", experience: "", ctc: "", location: ""
+  });
+  
+  const handleChange = (e) => {
+    const name = e.target.name;
+    const value = e.target.value;
+    setUser({ ...user, [name]: value });
+  }
+  
+  const submitForm = (e) => {
+    e.preventDefault();
     
+    if (editData) {
+      const updatedData = data.map((item) =>
+        item.phone === editData.phone ? user : item
+      );
+      setData(updatedData);
+      localStorage.setItem('data', JSON.stringify(updatedData));
+      
+    } else {
+      setData([...data, user]);
+      localStorage.setItem('data', JSON.stringify([...data, user]));
+      navigate('/list')
+    }
+    console.log(editData , 'modal')
+  }
 
-    return (
+  return (
+
     <div className='container'>
       <a href="/list">List</a>
-        <h1>React Form</h1>
+      <h1>React Form</h1>
       <form onSubmit={submitForm} >
         <label>Username:</label>
         <input type="text" name="username" value={user?.username} onChange={handleChange} required></input><br></br>
@@ -49,43 +51,45 @@ function Formm(EditData) {
         <label>Password:</label>
         <input type="password" name="password" value={user?.password} onChange={handleChange} required></input><br></br>
         <label>Gender</label>
-        <select name="gender">
-          <option value={user?.gender}>Male</option>
-          <option value={user?.gender}>Female</option>
-          <option value={user?.gender}>Not willing to disclose</option>
+        <select name="gender" value={user?.gender} onChange={handleChange} required>
+          <option value="none">None</option>
+          <option value="male">Male</option>
+          <option value="female">Female</option>
+          <option value="Notdisclose">Not willing to disclose</option>
         </select>
         <label>Experience In Years</label>
-        <select name="experience" value={user?.experience} onChange={handleChange}>
-          <option value={user?.experience}>Fresher</option>
-          <option value={user?.experience}>0-2 Years</option>
-          <option value={user?.experience}>2-4 Years</option>
-          <option value={user?.experience}>4-7 Years</option>
-          <option value={user?.experience}>8-10 Years</option>
-          <option value={user?.experience}>10+ Years</option>
-          <option value={user?.experience}>12+ Years</option>
+        <select name="experience" value={user?.experience} onChange={handleChange} required>
+          <option value="none">None</option>
+          <option value="fresher">Fresher</option>
+          <option value="0-2">0-2 Years</option>
+          <option value="2-4">2-4 Years</option>
+          <option value="4-7">4-7 Years</option>
+          <option value="8-10">8-10 Years</option>
+          <option value="10+">10+ Years</option>
+          <option value="12+">12+ Years</option>
         </select><br></br>
         <label>Expected CTC(In Lakhs per Annum)</label>
-        <select name="ctc">
-        <option value={user?.ctc}>None</option>
-          <option value={user?.ctc}>3-5</option>
-          <option value={user?.ctc}>5-7</option>
-          <option value={user?.ctc}>7-10</option>
-          <option value={user?.ctc}>10-15</option>
+        <select name="ctc" value={user?.ctc} onChange={handleChange} required>
+          <option value="none">None</option>
+          <option value="3-5">3-5</option>
+          <option value="5-7">5-7</option>
+          <option value="7-10">7-10</option>
+          <option value="10-15">10-15</option>
         </select>
         <label>Preferred Location</label>
-        <select name="location">
-        <option value={user?.location}>None</option>
-        <option value={user?.location}>Hyderabad</option>
-        <option value={user?.location}>Bangalore</option>
-        <option value={user?.location}>Chennai</option>
+        <select name="location" value={user?.location} onChange={handleChange} required>
+          <option value="none">None</option>
+          <option value="Hyderabad">Hyderabad</option>
+          <option value="Bangalore">Bangalore</option>
+          <option value="Chennai">Chennai</option>
         </select>
-        <button type="submit" > {EditData?.editData ? "update" : "Login"}</button>
-        
+        <button type="submit" > {editData ? "update" : "Login"}</button>
+
       </form>
-      </div>
-      
-        
-    
+    </div>
+
+
+
   );
 }
 
--- usermanagement/src/Components/TargetPage.js
+++ usermanagement/src/Components/TargetPage.js
@@ -1,4 +1,4 @@
-import React,{useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import Popup from 'reactjs-popup';
 import 'reactjs-popup/dist/index.css';
@@ -7,39 +7,39 @@ import Formm from "./Formm"
 function TargetPage() {
   const storedData = localStorage.getItem('data');
   const data = JSON.parse(storedData);
-    const [totalUsers, setTotalUsers] = useState([])
-    const [isEdit, setIsEdit] = useState(false);
-    const [editData, seteditData] = useState({})
-    useEffect(() => {
-        setTotalUsers(data);
-    }, [data]);
-  
-    const handleEdit = (user) =>{
-        // console.log(user);
-        setIsEdit(true)
-        seteditData(user);
-        const filteredUsers = totalUsers.filter((userDetails) => userDetails.username !== user.username);
-        setTotalUsers(filteredUsers);
-    }
-    
-    const handleDelete = (user) => {
-      const filteredUsers = totalUsers.filter((userDetails) => userDetails.phone !== user.phone);
-      setTotalUsers(filteredUsers);
-      localStorage.setItem('data', JSON.stringify(filteredUsers));
-    };
-    
+  const [totalUsers, setTotalUsers] = useState([])
+  const [isEdit, setIsEdit] = useState(false);
+  const [editData, seteditData] = useState({})
+
+  useEffect(() => {
+    setTotalUsers(data);
+  }, []);
+
+  const handleEdit = (user) => {
+    setIsEdit(true)
+    seteditData(user);
+    const filteredUsers = totalUsers.filter((userDetails) => userDetails.email !== user.email);
+    setTotalUsers(filteredUsers);
+  }
+
+  const handleDelete = (user) => {
+    const filteredUsers = totalUsers.filter((userDetails) => userDetails.phone !== user.phone);
+    setTotalUsers(filteredUsers);
+    localStorage.setItem('data', JSON.stringify(filteredUsers));
+  };
+
   return (
-    
+
     <>
-    <nav>
-      <ul>
-        <li>
-          <Link to="/list">List</Link>
-        </li>
-      </ul>
-    </nav>
-   
-    <div>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/list">List</Link>
+          </li>
+        </ul>
+      </nav>
+
+      <div>
         <table border="1">
           <thead>
             <tr>
@@ -47,8 +47,10 @@ function TargetPage() {
               <th>EmailId</th>
               <th>Password</th>
               <th>Phone No</th>
-              <th >Edit</th>
-              <th>Delete</th>
+              <th>Gender</th>
+              <th>Experience</th>
+              <th>CTC</th>
+              <th>Location</th>
             </tr>
           </thead>
           <tbody>
@@ -58,21 +60,26 @@ function TargetPage() {
                 <td>{item?.email}</td>
                 <td>{item?.password}</td>
                 <td>{item?.phone}</td>
-                <td onClick={()=>handleEdit(item)}>Edit</td>
-              <td onClick={()=>handleDelete(item)}>Delete</td>
+                <td>{item?.gender}</td>
+                <td>{item?.experience}</td>
+                <td>{item?.ctc}</td>
+                <td>{item?.location}</td>
+                <button  class="btn btn-success" onClick={() => handleEdit(item)}>Edit</button>
+                <button class="btn btn-danger" onClick={() => handleDelete(item)}>Delete</button>
               </tr>
             ))}
           </tbody>
         </table>
-        </div>
-        <Popup open={isEdit} onClose={() => setIsEdit(false)}>
+        <a href="http://localhost:3000/">Back</a>
+      </div>
+      <Popup open={isEdit} onClose={() => setIsEdit(false)}>
         <div className="popup-container">
           <div className="popup-content">
             <Formm editData={editData} setIsEdit={setIsEdit} />
           </div>
         </div>
       </Popup>
-        </>
+    </>
   );
 }
 
